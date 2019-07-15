d6cf2703d831b8b8056bc314419f41d7f8503f35
Klemek <klemek@outlook.fr>
2019-07-15 09:27:05 +0200

    Fixed failing test

--- test/rendering.test.js
+++ test/rendering.test.js
@@ -115,7 +115,7 @@ describe('toXML', () => {
         }
       }
     }, {w: 100, h: 100});
-    expect(res).toEqual('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="200" height="200"><circle cx="50" cy="50" r="50"/></svg>');
+    expect(res).toEqual('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="500" height="500"><circle cx="50" cy="50" r="50"/></svg>');
   });
 });
 
